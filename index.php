2793d1242761c6399a18411688334bb8bd1231ad
RomainDoukhan <89915670+RomainDoukhan@users.noreply.github.com>
2021-09-28 00:44:18 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 
 $nameMovie = "Indiana Jones and the last crusade" ;
-
 $showMovie = true ;
-
 $yearMovie = 1989 ;
-
-
 $imdbRating = 8.2 ; 
+
+echo $nameMovie ;
+echo $showMovie ; 
+echo $yearMovie ; 
+echo $imdbRating ; 
 
 ?>
